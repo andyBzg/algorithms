5bce46022965bb2265ecfb7f4f4033a73dacc62b
andyBzg <andr.bzg@gmail.com>
2023-02-02 22:55:34 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,18 +6,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int[] array = new int[]{3, 6, 4, 7, 2, 1, 9};
+        System.out.println(Arrays.toString(array));
         System.out.println("Enter the number");
         int number = scanner.nextInt();
 
         for (int i = 0; i < array.length; i++) {
             if (array[i] == number) {
                 System.out.println("Index of the number " + number + " is " + i);
-                break;
             }
         }
 
         //Реализовать алгоритм, который будет находить сумму квадратов всех элементов массива!
         int[] secondArray = new int[]{3, 6, 4, 7, 2, 1, 9};
+        System.out.println(Arrays.toString(secondArray));
         int sum = 0;
         for (int j : secondArray) {
             sum += j * j;
