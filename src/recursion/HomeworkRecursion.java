fd67005f203c72e68816d42f0523028a2b1dbfce
andyBzg <andr.bzg@gmail.com>
2023-02-16 23:03:56 +0100

    commit

--- src/recursion/HomeworkRecursion.java
+++ src/recursion/HomeworkRecursion.java
@@ -1,12 +1,11 @@
 package recursion;
 
-import java.util.Scanner;
-
-import static recursion.HomeworkRecursion.recursion;
+import static recursion.HomeworkRecursion.powerOfTwo;
+import static recursion.HomeworkRecursion.sumOfDigits;
 
 public class HomeworkRecursion {
 
-    public static int recursion(double n) {
+    public static int powerOfTwo(double n) {
         if (n == 1) {
             return 1;
         }
@@ -14,21 +13,38 @@ public class HomeworkRecursion {
             return 0;
         }
         else {
-            return recursion(n / 2);
+            return powerOfTwo(n / 2);
+        }
+    }
+
+
+    public static int sumOfDigits(int n)
+    {
+        if (n < 10) {
+            return n;
+        }
+        else {
+            return (n % 10) + sumOfDigits(n / 10);
         }
     }
 
 }
+
+
 class HomeworkRecursionMain {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        double n = scanner.nextInt();
-        if (recursion(n) == 1) {
+        double n = 8192;
+
+        if (powerOfTwo(n) == 1) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
         }
+
+
+        int sum = sumOfDigits((int) n);
+        System.out.println(sum);
 
     }
 
